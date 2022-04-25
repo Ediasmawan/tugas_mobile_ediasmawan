891215c7e44d2ae9d6addbf020f1824d02997f82
Ediasmawan <99790849+Ediasmawan@users.noreply.github.com>
2022-04-25 09:42:59 -0700

    Add files via upload

---
+++ imageclass.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DEDI AHYAR',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('edi asmawan_D0219322'),
+      ),
+      body: Center(
+        child: Image(image: AssetImage('assets/profile.png')),
+      ),
+    );
+  }
+}
